076b10be65cf3bf2f4c9c5d904e72dfafeee5c7b
Ethan1xin <ethanzang22@gmail.com>
2021-03-05 11:42:33 +1100

    getting ready to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) { # Assumed y as numeric
+                x <<- y  # Assign value to the parent environment
+                inv <<- NULL 
+        }
+        get <- function() x # Retrieve x from the parent environment
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set, get = get, # Assign each of these functions as an element within the list
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        dat <- x$get()
+        inv <- solve(dat) # return the inverse of x, or calculate & return if cache is empty
+        x$setInverse(inv)
+        inv
 }
